9f6a3f8997df0ff96ccf600c95426045620c9788
Nick <nick.buscemi13@gmail.com>
2023-07-26 10:09:02 -0400

    phase 3 complete

--- sql-practice/phase-3/query-1.sql
+++ sql-practice/phase-3/query-1.sql
@@ -1,2 +1,8 @@
 -- Find Hermione's cats
 -- Your code here
+SELECT cats.name
+FROM cats
+JOIN cat_owners ON cats.cat_id = cat_owners.cat_id
+JOIN owners ON cat_owners.owner_id = owners.owner_id
+WHERE owners.first_name = 'Hermione';
+
--- sql-practice/phase-3/query-2.sql
+++ sql-practice/phase-3/query-2.sql
@@ -1,2 +1,8 @@
 -- Find All the Toys for Hermione's cats
 -- Your code here
+
+SELECT toys.name 
+FROM toys 
+JOIN cat_owners ON toys.cat_id = cat_owners.cat_id
+JOIN owners ON cat_owners.owner_id = owners.owner_id
+WHERE owners.first_name = 'Hermione';
