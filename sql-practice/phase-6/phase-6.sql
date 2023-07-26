1c24f989528be80d8835295801d2ceed5ca38293
Nick <nick.buscemi13@gmail.com>
2023-07-26 11:09:28 -0400

    phase 6 complete

--- sql-practice/phase-6/phase-6.sql
+++ sql-practice/phase-6/phase-6.sql
@@ -1,2 +1,10 @@
 -- Update the toy with the name of "Cheetos" to have a name of "Pooky"
 -- Your code here
+UPDATE toys
+SET name = 'Pooky'
+WHERE toy_id = (
+  SELECT toy_id
+  FROM toys
+  JOIN cats ON toys.cat_id = cats.cat_id
+  WHERE toys.name = 'Cheetos' AND cats.name = 'Garfield'
+);
