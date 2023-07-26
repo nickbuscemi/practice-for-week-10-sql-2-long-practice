f714f7772640a146b9c431cc8dd4328df1ed0eef
Nick <nick.buscemi13@gmail.com>
2023-07-26 11:03:01 -0400

    phase 5 complete

--- sql-practice/phase-5/query-3.sql
+++ sql-practice/phase-5/query-3.sql
@@ -1,2 +1,7 @@
 -- Find the name of the cats who have an owner whose first name begins with an "H"
 -- Your code here
+SELECT cats.name 
+FROM cats
+JOIN cat_owners ON cats.cat_id = cat_owners.cat_id
+JOIN owners ON cat_owners.owner_id = owners.owner_id
+WHERE owners.first_name LIKE 'H%';
--- sql-practice/phase-5/query-5.sql
+++ sql-practice/phase-5/query-5.sql
@@ -1,2 +1,7 @@
 -- Find names of the cats whose owners are both George Beatty and Melynda Abshire, or just George Beatty, or just Melynda Abshire
 -- Your code here
+SELECT cats.name
+FROM cats
+JOIN cat_owners ON cats.cat_id = cat_owners.cat_id
+JOIN owners ON cat_owners.owner_id = owners.owner_id
+WHERE owners.first_name || ' ' || owners.last_name IN ('George Beatty', 'Melynda Abshire');
